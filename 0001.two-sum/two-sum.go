42c6a08d42d76c8494d44922b64b5da953837a9d
徐維謙 <jameshwc@gmail.com>
2019-12-21 19:19:50 +0800

    improve performance in 0001 two sum

--- 0001.two-sum/two-sum.go
+++ 0001.two-sum/two-sum.go
@@ -1,9 +1,14 @@
 package problem0001
 
+/*
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Two Sum.
+Memory Usage: 3.4 MB, less than 46.15% of Go online submissions for Two Sum.
+*/
+
 func twoSum(nums []int, target int) []int {
-	var index = make(map[int](int))
-	for i, l := 0, len(nums); i < l; i++ {
-		if n, ok := index[target-nums[i]]; ok {
+	var index = make(map[int](int), len(nums))
+	for i, v := range nums {
+		if n, ok := index[target-v]; ok {
 			return []int{n, i}
 		}
 		index[nums[i]] = i
