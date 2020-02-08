06af8902cf422f256d09070be6072ce3b2314173
jameshwc <b07902001@csie.ntu.edu.tw>
2020-02-08 13:19:59 +0800

    0104 accepted

---
+++ algorithm/0104.maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
