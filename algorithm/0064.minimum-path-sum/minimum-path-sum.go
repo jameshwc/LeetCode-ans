7c8b0abccb20d2ba0ffe9f0a39b5b73925949194
jameshwc <b07902001@csie.ntu.edu.tw>
2020-02-09 11:24:42 +0800

    0064 accepted

---
+++ algorithm/0064.minimum-path-sum/minimum-path-sum.go
@@ -0,0 +1,26 @@
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			var min int
+			if dp[i-1][j] > dp[i][j-1] {
+				min = dp[i][j-1]
+			} else {
+				min = dp[i-1][j]
+			}
+			dp[i][j] = min + grid[i][j]
+		}
+	}
+	return dp[m-1][n-1]
+}
