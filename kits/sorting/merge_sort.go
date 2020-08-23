75c060cb4480a29f09da9f16110adaa6cafe1474
Vulpic <jameshwc@gmail.com>
2020-08-23 22:44:50 +0800

    add kits/sorting, finish merge sort & quick sort

---
+++ kits/sorting/merge_sort.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func merge(nums []int) {
+	n := len(nums)
+	if n <= 1 {
+		return
+	}
+	merge(nums[:n/2])
+	merge(nums[n/2:])
+	i, j := 0, n/2
+	cur := 0
+	sortNum := make([]int, len(nums))
+	for {
+		if nums[i] > nums[j] {
+			sortNum[cur] = nums[j]
+			j++
+			cur++
+		} else {
+			sortNum[cur] = nums[i]
+			i++
+			cur++
+		}
+		if i == n/2 {
+			for ; j < len(nums); j++ {
+				sortNum[cur] = nums[j]
+				cur++
+			}
+			break
+		}
+		if j == len(nums) {
+			for ; i < n/2; i++ {
+				sortNum[cur] = nums[i]
+				cur++
+			}
+			break
+		}
+	}
+	for i := range nums {
+		nums[i] = sortNum[i]
+	}
+}
+func mergeSort(nums []int) {
+	merge(nums)
+}
+
+func main() {
+	nums := []int{1, 7, 2, 3, 5, 10, 8, 9, 12, 3, 2, 4}
+	mergeSort(nums)
+	fmt.Println(nums)
+}
---
+++ kits/sorting/quick_sort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func quickSort(nums []int, left, right int) {
+	if right < left {
+		return
+	}
+	pivot := nums[right]
+	cur := left
+	for i := left; i < right; i++ {
+		if nums[i] < pivot {
+			nums[i], nums[cur] = nums[cur], nums[i]
+			cur++
+		}
+	}
+	nums[right], nums[cur] = nums[cur], nums[right]
+	quickSort(nums, left, cur-1)
+	quickSort(nums, cur+1, right)
+}
+
+func main() {
+	nums := []int{1, 7, 2, 3, 5, 10, 8, 9, 12, 3, 2, 4}
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println(nums)
+}
