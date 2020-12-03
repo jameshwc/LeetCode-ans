c6abcb0cea888fee84bb387ca04fe39385299056
jameshwc <jameshwc@gmail.com>
2020-12-03 23:00:16 +0800

    0326 accepted

---
+++ algorithm/0326.power-of-three/power-of-three.go
@@ -0,0 +1,14 @@
+
+// TODO: research other ways
+func isPowerOfThree(n int) bool {
+	if n == 0 {
+		return false
+	}
+	if n == 1 {
+		return true
+	}
+	if n%3 != 0 {
+		return false
+	}
+	return isPowerOfThree(n / 3)
+}
