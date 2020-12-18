b0d70df3c98b79b02148ab80cad3b93f837ea593
jameshwc <jameshwc@gmail.com>
2020-12-18 08:19:36 +0800

    0328 accepted

---
+++ algorithm/0328.odd-even-linked-list/odd-even-linked-list.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	cur := head
+	oddCur := head.Next
+
+}
