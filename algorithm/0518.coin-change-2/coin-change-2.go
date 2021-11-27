ab9b65fe3e276fb352bcf80417c13d4f3a776b1e
Vulpic <jameshwc@gmail.com>
2021-11-27 08:08:59 +0800

    0518

---
+++ algorithm/0518.coin-change-2/coin-change-2.go
@@ -0,0 +1,10 @@
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+	for i := range coins {
+		for j := coins[i]; j <= amount; j++ {
+			dp[j] += dp[j-coins[i]]
+		}
+	}
+	return dp[amount]
+}
